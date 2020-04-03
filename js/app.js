10868055f3340ed9bd8147097d08f255832cd8c6
timursaet <s.timursaet@yandex.ru>
2020-04-03 17:31:10 +0500

    add span for view data

--- js/app.js
+++ js/app.js
@@ -6,20 +6,26 @@ const element = showModal.querySelectorAll("div, div>img, div>span");
 users.forEach((user) => {
    user.addEventListener('click', (event) => {
     event.stopPropagation();
-    const { name, phone, photo, bdday, online} = event.currentTarget.dataset;
-        element[0].innerHTML += online;
-        element[3].innerHTML = name;
-        element[5].innerHTML += bdday;
-        element[7].innerHTML += phone;
-        element[9].src = photo;
+    const { name, phone, photo, bdday, online } = event.currentTarget.dataset;
+        element[1].innerHTML = online;
+        element[4].innerHTML = name;
+        element[6].innerHTML = bdday;
+        element[8].innerHTML = phone;
+        element[10].src = photo;
         showModal.style.display = "grid";
    });
 });
 
 document.addEventListener('click', () => {
     showModal.style.display = "none";
+    document.removeEventListener('click',() => {
+        console.log('Обработчик удален!')
+    });
 });
 
 close.addEventListener('click', () => {
      showModal.style.display = "none";
+     document.removeEventListener('click',() => {
+        console.log('Обработчик удален!')
+    });
 });
