25b4825f3506fcff4fe091f8a489ab23a92186b6
timursaet <s.timursaet@yandex.ru>
2020-04-04 00:44:26 +0500

    add friends

--- js/app.js
+++ js/app.js
@@ -3,16 +3,21 @@ const showModal = document.querySelectorAll('.container .showModal')[0];
 const close = document.querySelectorAll('.container .showModal .showModal__exit')[0];
 const element = showModal.querySelectorAll("div, div>img, div>span"); 
 
+let countFriend = 0;
+
 users.forEach((user) => {
-   user.addEventListener('click', (event) => {
-    event.stopPropagation();
-    const { name, phone, photo, bdday, online } = event.currentTarget.dataset;
-        element[1].innerHTML = online;
-        element[4].innerHTML = name;
-        element[6].innerHTML = bdday;
-        element[8].innerHTML = phone;
-        element[10].src = photo;
-        showModal.style.display = "grid";
+    countFriend++;
+    user.addEventListener('click', (event) => {
+        event.stopPropagation();
+        const { name, phone, photo, bdday, online } = event.currentTarget.dataset;
+            element[1].innerHTML = online;
+            element[4].innerHTML = name;
+            element[6].innerHTML = bdday;
+            element[8].innerHTML = phone;
+            element[10].src = photo;
+            element[13].innerHTML = `${countFriend} друзей`;
+            event.currentTarget.appendChild(showModal);
+            showModal.style.display = "grid";
    });
 });
 
