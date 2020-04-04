9b46416a2e2b141d1ecd44904ecd0a3e3e4197c4
timursaet <s.timursaet@yandex.ru>
2020-04-05 00:46:21 +0500

    add setTimeout

--- js/drawModal.js
+++ js/drawModal.js
@@ -1,8 +1,7 @@
 const users_container = document.querySelectorAll('.container .user');
 
-function render(h) {
-        let showModal = `<div class="showModal">
-                        <div class="showModal__main">
+function render(block) {
+        let showModal = `<div class="showModal__main">
                             Была в сети в 
                             <span>${this.online}</span>
                         </div>
@@ -19,23 +18,31 @@ function render(h) {
                             <span class="showModal_data">${this.phone}</span>
                         </div>
                         <div class="showModal__img">
-                            <img src="${this.img}" width="75px" height="75px"/>
+                            <img src="${this.photoSrc}" width="75px" height="75px"/>
                         </div>
                         <div class="showModal__other">
                             <img src="img/sendMessage.jpg" class="showModal__other_img"  title="Отправить сообщение" 
                             alt="Отправить сообщение"/>
                             <span class="showModal_data"></span>
                         </div>`;
-        h.innerHtml = showModal;
-        event.currentTarget.appendChild(showModal);
+        block.innerHTML = showModal;
 };
 
+const showModal = document.querySelectorAll('.container .showModal')[0];
+
 users_container.forEach((item) => {
-    item.addEventListener('click', (event) => {
+    item.onmouseover = function() {     
         const { id } = event.currentTarget.dataset;
-        const user = users.find(user => user.id == id);
-        if (user) {
-            render.call(user, container);
-        };
-    });
+        setTimeout(() => {
+            const user = users.find(user => user.id == id);
+            if (user) {
+                render.call(user, showModal);
+                showModal.style.display = "grid";
+            };
+        }, 1000);
+        event.currentTarget.appendChild(showModal);
+    }
+    item.onmouseout = function(event) {
+        showModal.style.display = "none";
+    }
 });
--- js/users.js
+++ js/users.js
@@ -6,7 +6,7 @@ const users = [
         dateOfBirth: '22.09.1997',
         education: 'БГУ',
         course: '4',
-        phone: +7-937-158-05-08,
+        phone: '+7-937-158-05-08',
         photoSrc: 'img/ava1.png'
     },
     {
@@ -16,7 +16,7 @@ const users = [
         dateOfBirth: '01.10.2001',
         education: 'УГАТУ',
         course: '3',
-        phone: +7-888-953-14-74,
+        phone: '+7-888-953-14-74',
         photoSrc: 'img/ava2.png'
     },
 ];
