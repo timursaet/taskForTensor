3a37f510978cdc99bf4d9889a786c5692f334fbc
timursaet <s.timursaet@yandex.ru>
2020-04-05 13:33:11 +0500

    add get for computing year

--- js/app.js
+++ js/app.js
@@ -10,11 +10,9 @@ class Student {
         this.photoSrc = user.photoSrc;
         this.dateOfBirth = user.dateOfBirth;
     }
-    get year() {
-        let number = this.dateOfBirth.split('.'),
-            currentTime = new Date(),
-            time = new Date(number[2], number[1], number[0]);
-        return Math.floor((currentTime-time) / (60*60*24*1000*365));
+    get age() {
+        let number = this.dateOfBirth.split('.');
+        return Math.floor((new Date()-new Date(number[2], number[1], number[0])) / (60*60*24*1000*365)) + ' года';
     }
     render(block) {
         let view = `<div class="user" data-id="${this.id}">
@@ -28,7 +26,10 @@ class Student {
     }
 }
 
+let countFriends = 0;
+
 users.forEach((item) => {
+    countFriends++;
     let student = new Student(item);
     student.render(container);
 });
